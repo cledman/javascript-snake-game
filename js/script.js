595d7adb60e8df53a4bb043d24652d726ca0c03f
Cledson Lopes <cledman@gmail.com>
2020-09-27 12:06:42 -0300

    Controller implemented

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "right";
+let game = setInterval(startGame, 100);
 
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
+
 function createBG() {
   context.fillStyle = "Lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +22,38 @@ function createSnake() {
   }
 }
 
-createBG();
-createSnake();
+document.addEventListener("keydown", update);
+
+function update(event) {
+  console.log(event.keyCode);
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+function startGame() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "lup") snake[0].y = 16 * box;
+
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+  //console.log(direction);
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
